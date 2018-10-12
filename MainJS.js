fd73fc6f1fa483c376f395f6df6a2b1129a7897f
MubashirEbad <mubashir.ebad@gmail.com>
2018-10-12 12:50:14 +0500

    library management system

---
+++ MainJS.js
@@ -0,0 +1,12 @@
+var arrayofBooks =
+	[
+		["Frankenstein", "Author1", "Publisher1", "2018-10-01"],
+		["Book2", "Author2", "Publisher2", "2018-10-02"],
+		["Book3", "Author3", "Publisher3", "2018-10-03"],
+		["Book4", "Author4", "Publisher4", "2018-10-04"],
+		["Cracking the Tech Career, 2nd Edition", "Author5", "Publisher5", "2018-10-05"],
+	];
+
+function getArray() {
+	return this.arrayofBooks;
+}
